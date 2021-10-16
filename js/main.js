939c23f912226cf88210a1366b52464eb4fcaa0b
valfett <vallemork@gmail.com>
2021-10-16 18:55:04 +0200

    1

--- js/main.js
+++ js/main.js
@@ -83,11 +83,12 @@ async function main(){
     
     // fps counter
     let stats = new Stats()
-    stats.showPanel(0)
-    document.body.append(stats.dom)
-    stats.dom.style.left = ""
-    stats.dom.style.right = "0px"  
-    stats.dom.setAttribute("class", "stats")
+    console.log(stats)
+    const statsDom = stats.domElement
+    document.body.append(statsDom)
+    statsDom.style.left = ""
+    statsDom.style.right = "0px"  
+    statsDom.setAttribute("class", "stats")
 
     //navbar
     const navbar = document.querySelector(".navbar")
